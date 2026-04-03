68348a0fdcfbb79decc6ec1bdc46877e78844d02
skinJack97-tech <vyskubovi97@gmail.com>
2026-04-04 03:46:18 +0500

    Update Package.swift for compatibility and dependencies
    Updated Swift tools version and lowered iOS platform version for compatibility. Changed dependency branch from 'master' to 'main'.

--- iOS/Package.swift
+++ iOS/Package.swift
@@ -1,16 +1,17 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "AyuGram",
     platforms: [
-        .iOS(.v26)
+        .iOS(.v15) // Понижаем до iOS 15 для максимальной совместимости
     ],
     products: [
         .library(name: "AyuGram", targets: ["AyuGram"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Swiftgram/TDLibKit", branch: "master"),
+        // Меняем branch: "master" на branch: "main", так как GitHub теперь использует main
+        .package(url: "https://github.com/Swiftgram/TDLibKit", branch: "main"),
     ],
     targets: [
         .target(
