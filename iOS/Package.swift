e61890963696e22cfaf96076cc07080efeb70667
skinJack97-tech <vyskubovi97@gmail.com>
2026-04-04 03:52:33 +0500

    Update Package.swift

--- iOS/Package.swift
+++ iOS/Package.swift
@@ -4,13 +4,12 @@ import PackageDescription
 let package = Package(
     name: "AyuGram",
     platforms: [
-        .iOS(.v15) // Понижаем до iOS 15 для максимальной совместимости
+        .iOS(.v15) 
     ],
     products: [
         .library(name: "AyuGram", targets: ["AyuGram"])
     ],
     dependencies: [
-        // Меняем branch: "master" на branch: "main", так как GitHub теперь использует main
         .package(url: "https://github.com/Swiftgram/TDLibKit", branch: "main"),
     ],
     targets: [
